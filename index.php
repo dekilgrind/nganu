b5317f5f7fc16c3f4bf288c0ffb8ea3ab2ffd619
dekilgrind <32001529+dekilgrind@users.noreply.github.com>
2018-03-25 11:44:50 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,8 +7,8 @@ And Modified by Farzain - zFz ( Faraaz )
 */
 require_once('./line_class.php');
 
-$channelAccessToken = 'ACCESS TOKEN ENTE'; //Your Channel Access Token
-$channelSecret = 'SECRET TOKEN ENTE';//Your Channel Secret
+$channelAccessToken = 'BIgjEJ10YTGLTeSgeI17mwDOHjltpqM9eSwa3Vd8OBy1DMKOieslwiEM7hx/G+HUAKPCzzeGpTpyTLkB7AABQ1U5+NsTwK/dp8EingGgIIl7gLglcl0MVYUUf0H4V5zPuqNGdUXHN0gW4G/FJA1kdAdB04t89/1O/w1cDnyilFU='; //Your Channel Access Token
+$channelSecret = '68325863c8d4ba7acf0cc2d4699bfc49';//Your Channel Secret
 
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
